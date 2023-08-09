4b0a2fdc4d8cf8b05e2433b341a63041290896b4
HERO Kwon <herokwon@gmail.com>
2023-08-09 20:13:34 +0900

    home: v3_re

--- coating_modulus/a_navier_stokes_vof_2d_v4.py
+++ coating_modulus/a_navier_stokes_vof_2d_v4.py
@@ -119,9 +119,9 @@ class NavierStokes_VOF(PDE):
         self.L_ref = L_ref
         self.rho_ref = rho_ref
 
-        mu = (mu2 + (mu1 - mu2) * a ) / vis_ref
-        mu_x = (mu1 - mu2) * a.diff(x) / vis_ref
-        mu_y = (mu1 - mu2) * a.diff(y) / vis_ref
+        mu = (mu2 + (mu1 - mu2) * a )
+        mu_x = (mu1 - mu2) * a.diff(x)
+        mu_y = (mu1 - mu2) * a.diff(y)
         rho = rho2 + (rho1 - rho2) * a        
 
         abs_interface_grad = sqrt(Pow(a.diff(x),2) + Pow(a.diff(y),2) + np.finfo(float).eps)
